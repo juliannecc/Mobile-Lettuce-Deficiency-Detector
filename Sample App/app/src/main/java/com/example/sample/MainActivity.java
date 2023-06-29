48679f131894ae8c9ba9bf59acd74217f003cd2c
KirkStarfish <patrick.formales@eee.upd.edu.ph>
2023-06-29 16:58:43 +0800

    integration code in android studio

--- Sample App/app/src/main/java/com/example/sample/MainActivity.java
+++ Sample App/app/src/main/java/com/example/sample/MainActivity.java
@@ -8,39 +8,34 @@ import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.example.sample.ml.Vggseg;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.provider.MediaStore;
-import android.service.controls.templates.ThumbnailTemplate;
 import android.util.Log;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.sample.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.model.Model;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     TextView Result;
     Bitmap bitmap;
     // Mat src;
+    int imageSize =  256;
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -93,6 +89,76 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public TensorBuffer segmentImage(Bitmap image) {
+        TensorBuffer outputFeature0 = null;
+        try {
+            Vggseg model = Vggseg.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 256, 256, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Vggseg.Outputs outputs = model.process(inputFeature0);
+            outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+
+        return outputFeature0;
+    }
+
+    public void classifyImage(Bitmap image){
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 256, 256, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+            int [] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f/1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f/1));
+                    byteBuffer.putFloat((val & 0xFF)  * (1.f/1));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -101,69 +167,83 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap image = (Bitmap) data.getExtras().get("data");
                 int dimension = Math.min(image.getWidth(), image.getHeight());
                 image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                ImageView.setImageBitmap(image);
 
-                Mat matRGB = new Mat();
-                Mat matLab = new Mat();
-                ArrayList<Mat> LabChannels = new ArrayList<>(3);
-
-                Mat maskA = new Mat();
-                Mat maskB = new Mat();
-                Mat maskAB = new Mat();
-
-                Mat dest = new Mat();
-
-                Utils.bitmapToMat(image, matRGB);
-
-                Imgproc.cvtColor(matRGB, matLab, Imgproc.COLOR_RGB2Lab);
-                Core.split(matLab, LabChannels);
-
-                Imgproc.threshold(LabChannels.get(1), maskA, 0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
-                Imgproc.threshold(LabChannels.get(2), maskB, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
-
-                Core.bitwise_or(maskA, maskB, maskAB);
-
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                Bitmap maskA = segmentImage(image);
+                Bitmap segmentedImage = [];
+                Core.bitwise_or(MaskA, image, segmentedImage);
                 Core.add(dest, Scalar.all(0), dest);
 
-                matRGB.copyTo(dest, maskAB);
 
-                Utils.matToBitmap(dest, image);
+
+//                Mat matRGB = new Mat();
+//                Mat matLab = new Mat();
+//                ArrayList<Mat> LabChannels = new ArrayList<>(3);
+//
+//                Mat maskA = new Mat();
+//                Mat maskB = new Mat();
+//                Mat maskAB = new Mat();
+//
+//                Mat dest = new Mat();
+//
+//                Utils.bitmapToMat(image, matRGB);
+//
+//                Imgproc.cvtColor(matRGB, matLab, Imgproc.COLOR_RGB2Lab);
+//                Core.split(matLab, LabChannels);
+//
+//                Imgproc.threshold(LabChannels.get(1), maskA, 0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
+//                Imgproc.threshold(LabChannels.get(2), maskB, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
+
+//                Core.bitwise_or(maskA, maskB, maskAB);
+//
+//                Core.add(dest, Scalar.all(0), dest);
+//
+//                matRGB.copyTo(dest, maskAB);
+//
+//                Utils.matToBitmap(dest, image);
                 ImageView.setImageBitmap(image);
             }else{
                 Uri dat = data.getData();
                 Bitmap image = null;
                 try {
                     image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                    ImageView.setImageBitmap(image);
 
-                    Mat matRGB = new Mat();
-                    Mat matLab = new Mat();
-                    ArrayList<Mat> LabChannels = new ArrayList<>(3);
+                    image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                    Mat maskA = segmentImage(image);
 
-                    Mat maskA = new Mat();
-                    Mat maskB = new Mat();
-                    Mat maskAB = new Mat();
-
-                    Mat dest = new Mat();
-
-                    Utils.bitmapToMat(image, matRGB);
-
-                    Imgproc.cvtColor(matRGB, matLab, Imgproc.COLOR_RGB2Lab);
-                    Core.split(matLab, LabChannels);
-
-                    Imgproc.threshold(LabChannels.get(1), maskA, 0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
-                    Imgproc.threshold(LabChannels.get(2), maskB, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
-
-                    Core.bitwise_or(maskA, maskB, maskAB);
-
-                    Core.add(dest, Scalar.all(0), dest);
-
-                    matRGB.copyTo(dest, maskAB);
-
-                    Utils.matToBitmap(dest, image);
+//                    Mat matRGB = new Mat();
+//                    Mat matLab = new Mat();
+//                    ArrayList<Mat> LabChannels = new ArrayList<>(3);
+//
+//                    Mat maskA = new Mat();
+//                    Mat maskB = new Mat();
+//                    Mat maskAB = new Mat();
+//
+//                    Mat dest = new Mat();
+//
+//                    Utils.bitmapToMat(image, matRGB);
+//
+//                    Imgproc.cvtColor(matRGB, matLab, Imgproc.COLOR_RGB2Lab);
+//                    Core.split(matLab, LabChannels);
+//
+//                    Imgproc.threshold(LabChannels.get(1), maskA, 0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
+//                    Imgproc.threshold(LabChannels.get(2), maskB, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
+//
+//                    Core.bitwise_or(maskA, maskB, maskAB);
+//
+//                    Core.add(dest, Scalar.all(0), dest);
+//
+//                    matRGB.copyTo(dest, maskAB);
+//
+//                    Utils.matToBitmap(dest, image);
                     ImageView.setImageBitmap(image);
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
