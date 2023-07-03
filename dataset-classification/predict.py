361b3523274326ab50bae2f65890aaf546dfe8e0
ja-ckieee <alquea.macatangay@eee.upd.edu.ph>
2023-07-03 15:26:56 +0800

    resized test-segmented

--- dataset-classification/predict.py
+++ dataset-classification/predict.py
@@ -8,8 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Folders
-source_name = 'validate-images'
-target_root = 'validate-segmented'
+source_name = 'test-images'
+target_root = 'test-segmented'
 
 # Load Model
 model = load_model('../vggseg.h5')
